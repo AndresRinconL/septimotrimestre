ca6276a49188babb9735629bec157146876796bb
AndresRinconL <49130288+AndresRinconL@users.noreply.github.com>
2021-02-06 11:16:05 -0500

    update primer proyecto

--- Desarrollo/entregar_domingo/primer-proyecto/resources/views/index.blade.php
+++ Desarrollo/entregar_domingo/primer-proyecto/resources/views/index.blade.php
@@ -1,13 +1,24 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Index</title>
 </head>
 <body>
-Hola desde INDEX
+<div class="card-body">
+    <div class="container">
+        <div class="row" >
+            <div class="botones">
+                <a href=""class="btn btn-info">Ficha</a>
+                <a href=""class="btn btn-dark">Programa</a>
+                <a href=""class="btn btn-info">Estudiante</a>
+                <a href=""class="btn btn-dark">Instructor</a>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
--- Desarrollo/entregar_domingo/primer-proyecto/routes/web.php
+++ Desarrollo/entregar_domingo/primer-proyecto/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\usercontroller;
-use App\Http\Controllers\vehiculoscontroller;
-use App\Http\Controllers\landingcontroller;
+use App\Http\Controllers\Estudiantecontroller;
+use App\Http\Controllers\fichacontroller;
+use App\Http\Controllers\programacontroller;
+use App\Http\Controllers\InstructorController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,40 +19,7 @@ use App\Http\Controllers\landingcontroller;
 Route::get('/', function () {
     return view('index');
 });
-
-//rutas Principales
-route::get( 'inicio',[LandingController::class,'inicio'])->name('user.index');
-route::get( 'inicio',[LandingController::class,'inicio'])->name('principal.inicio');
-route::get('vehiculo',[LandingController::class,'vehiculo'])->name('vehiculo.index');
-route::get('user',[LandingController::class,'user'])->name('vehiculo.index');
-route::get('informe',[landingcontroller::class,'informe'])->name('principal.informe');
-
-
-
-//rutas vehiculares
-Route::get('vehiculo',[vehiculoscontroller::class,'index'])->name('vehiculo.index');
-Route::get('vehiculo/create',[vehiculoscontroller::class,'create'])->name('vehiculo.create');
-Route::post('vehiculo',[vehiculoscontroller::class,'store'])->name('vehiculo.store');
-Route::get('vehiculo/{id}',[vehiculoscontroller::class,'show'])->name('vehiculo.show');
-Route::delete('vehiculo/{id}',[vehiculoscontroller::class,'destroy'])->name('vehiculo.destroy');
-Route::get('vehiculo/edit/{id}',[vehiculoscontroller::class,'edit'])->name('vehiculo.edit');
-Route::put('vehiculo/{id}',[vehiculoscontroller::class,'update'])->name('vehiculo.update');
-
-//rutas usuario
-Route::get('user',[usercontroller::class,'index'])->name('user.index');
-Route::get('user/create',[usercontroller::class,'create'])->name('user.create');
-Route::post('user',[usercontroller::class,'store'])->name('user.store');
-Route::get('user/{id}',[usercontroller::class,'show'])->name('user.show');
-Route::delete('user/{id}',[usercontroller::class,'destroy'])->name('user.destroy');
-Route::get('user/edit/{id}',[usercontroller::class,'edit'])->name('user.edit');
-Route::put('user/{id}',[usercontroller::class,'update'])->name('user.update');
-
-//rutas conductor
-
-Route::get('conductor',[usercontroller::class,'index'])->name('Conductor.index');
-Route::get('conductor/create',[usercontroller::class,'create'])->name('Conductor.create');
-Route::post('conductor',[usercontroller::class,'store'])->name('conductor.store');
-Route::get('conductor/{id}',[usercontroller::class,'show'])->name('Conductor.show');
-Route::delete('conductor/{id}',[usercontroller::class,'destroy'])->name('conductor.destroy');
-Route::get('conductor/edit/{id}',[usercontroller::class,'edit'])->name('Conductor.edit');
-Route::put('conductor/{id}',[usercontroller::class,'update'])->name('conductor.update');
+Route::resources('estudiante',Estudiantecontroller::class);
+Route::resources('estudiante',fichacontroller::class);
+Route::resources('estudiante',InstructorController::class);
+Route::resources('estudiante',programacontroller::class);
